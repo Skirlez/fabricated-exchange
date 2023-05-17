74f6a8e613a9edbd1b1676176af41b266f3344a2
Skirlez <skirlez3@gmail.com>
2023-05-17 20:12:32 +0300

    Initial commit

---
+++ src/main/java/com/skirlez/fabricatedexchange/FabricatedExchangeClient.java
@@ -0,0 +1,10 @@
+package com.skirlez.fabricatedexchange;
+
+import net.fabricmc.api.ClientModInitializer;
+
+
+public class FabricatedExchangeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
